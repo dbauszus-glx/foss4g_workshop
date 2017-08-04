d303f311394c0377347c41b4be3be2ebcb193351
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2017-08-04 17:33:04 +0100

    testing

--- public/js/workshop_entry.js
+++ public/js/workshop_entry.js
@@ -8,11 +8,11 @@ const map = L.map('map', {
     maxZoom: 15
 }).setView([54, -2], 6);
 
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png').addTo(map);
-map.createPane('labels');
-map.getPane('labels').style.zIndex = 650;
-map.getPane('labels').style.pointerEvents = 'none';
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_only_labels/{z}/{x}/{y}.png',{pane: 'labels'}).addTo(map);
+// L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png').addTo(map);
+// map.createPane('labels');
+// map.getPane('labels').style.zIndex = 650;
+// map.getPane('labels').style.pointerEvents = 'none';
+// L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_only_labels/{z}/{x}/{y}.png',{pane: 'labels'}).addTo(map);
 
 // map.createPane('grid');
 // map.getPane('grid').style.zIndex = 550;
