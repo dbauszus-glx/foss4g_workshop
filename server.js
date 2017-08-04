328cba941baf40caf6e01eb03976abbcf21976c7
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2017-08-04 15:27:23 +0100

    pgp-promise

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', require('./router'));
 app.listen(3000);
 console.log('The magic happens on port 3000');
+
+
+
+
+
